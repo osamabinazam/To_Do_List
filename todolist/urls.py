346546cf877203c68f8a62ae9590d31a5839758b
Osama Bin Azam <osamabinazam@gmail.com>
2023-04-21 10:11:58 +0500

    Class based View (DetailView, ListView)

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
-from . import views
+# from . import views
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
-    path('',views.home),
+    path('',TaskList.as_view(), name='task'),
+    path('task/<int:pk>/' , TaskDetail.as_view(), name='task-detail') ,
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from todolist.models import Task
 
 # Create your views here.
-def home(request):
-    return HttpResponse("<h1> Hello World </h1>")
+
+# Classed base view 
+class TaskList(ListView):
+    # Make Task Model availabe to template for rendering and other purpose
+    model = Task
+    #  Help to change the object name that sent to html document
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name ="tasks"
+    template_name = 'todolist/task.html'
