a6e86c9a677d73274c871fd7924490efe337be59
Osama Bin Azam <osamabinazam@gmail.com>
2023-04-21 08:58:38 +0500

    Modifications

--- To_Do_List/settings.py
+++ To_Do_List/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app.apps.py.TodolistConfig"
+    "todolist.apps.TodolistConfig"
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = "To_Do_List.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [ BASE_DIR / 'templates' ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
