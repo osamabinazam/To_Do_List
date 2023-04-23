36ffb7a951b07b79cf8249de82e2cf6033b46212
Osama Bin Azam <osamabinazam@gmail.com>
2023-04-23 11:16:36 +0500

    Task creation module using CreateView

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
 # from . import views
-from .views import TaskList, TaskDetail
+from .views import TaskList, TaskDetail,TaskCreate
 
 urlpatterns = [
     path('',TaskList.as_view(), name='task'),
     path('task/<int:pk>/' , TaskDetail.as_view(), name='task-detail') ,
+    path ('task-create/' , TaskCreate.as_view(), name="task-create"),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+
+# reverse_lazy function is used to generate URL based on the name of the view function or URL Pattern
+# It return a lazy object, which means that URL will not be resolved untill it is actually needed
+# Useful in situations where URL might not be needed immediately
+from django.urls import reverse_lazy
 from todolist.models import Task
 
 # Create your views here.
@@ -13,7 +19,19 @@ class TaskList(ListView):
     #  Help to change the object name that sent to html document
     context_object_name = 'tasks'
 
+
 class TaskDetail(DetailView):
     model = Task
     context_object_name ="tasks"
-    template_name = 'todolist/task.html'
+    template_name = 'todolist/task.html'        # Set custom name of the template
+
+class TaskCreate(CreateView):
+    model = Task
+
+    # field is used to display all field of the form on webpage
+    fields = "__all__"    
+    # 
+    # particular fields can be select
+    # fields = ['title', 'description']
+    success_url =  reverse_lazy("task")
+    
